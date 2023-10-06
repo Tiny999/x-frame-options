a02dfbca760f68a876baac05ac29d03a1342a233
Abbas Salami <olawalesalami3@gmail.com>
2023-10-06 17:52:25 +0100

    debug

--- index.js
+++ index.js
@@ -80,7 +80,6 @@
 
 // app.listen(port, () => console.log(`Listening on port ${port}!`));
 
-
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
@@ -94,41 +93,42 @@ const port = 3000;
  * @returns
  */
 function extractDomain(url) {
-    const match = url.match(
-        /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im
-    );
-    return match && match[0];
+  const match = url.match(
+    /^(?:https?:\/\/)?(?:[^@\n]+@)?(?:www\.)?([^:\/\n]+)/im
+  );
+  return match && match[0];
 }
 
 /**
  * Prepare final data
  */
 function prepareFinalData(data, domain) {
-    let finalData = data.replaceAll(/.(?<=="\/)(?<=.[^"]+)/gim, `${domain}/`);
-    return finalData;
+  let finalData = data.replaceAll(/.(?<=="\/)(?<=.[^"]+)/gim, `${domain}/`);
+  return finalData;
 }
 
 app.use(cors()); // Enable CORS for all routes
 
 app.get("/bypass/*", async (req, res) => {
-    const url = req.params[0];
-    const domain = extractDomain(url);
+  const url = req.params[0];
+  const domain = extractDomain(url);
 
-    try {
-        const response = await axios.get(url);
-        const html = response.data;
+  try {
+    const response = await axios.get(url);
+    const html = response.data;
 
-        /**
-         * Relative url should be absolute
-         */
+    console.log(html)
+    /**
+     * Relative url should be absolute
+     */
 
-        res.send(prepareFinalData(html, domain));
-    } catch (error) {
-        console.error(error);
-        res.status(500).send("Error occurred while fetching the URL");
-    }
+    res.send(prepareFinalData(html, domain));
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error occurred while fetching the URL");
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`);
+  console.log(`Example app listening on port ${port}`);
 });
